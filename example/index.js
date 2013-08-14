e215f9117370d9751309ad871b552915c18efe96
Mitchel Kelonye <kelonyemitchel@gmail.com>
2013-08-14 14:13:33 +0300

    refactor

--- example/index.js
+++ example/index.js
@@ -6,14 +6,11 @@ app.use(express.logger('dev'));
 app.use(express.bodyParser());
 app.use(express.static(__dirname + '/../public'));
 app.get('/', function(req, res) {
-  return res.sendfile(__dirname + '/view.html');
+  res.sendfile(__dirname + '/view.html');
 });
 app.post('/upload', function(req, res) {
   var file = req.files.file;
-  if (file) {
-    return res.send(200);
-  }
-  res.send(400);
+  res.send(!!!file?200:400);
 });
 
 app.listen(3000);
